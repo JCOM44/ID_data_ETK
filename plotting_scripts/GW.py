ccb0cfadd10d86ad96ba9c444f0f94ac395eeabb
JCOM44_BINAC <jose.olvera-meneses@uni-tuebingen.de>
2024-09-24 14:56:27 +0200

    adding file for plotting GW

--- plotting_scripts/GW.py
+++ plotting_scripts/GW.py
@@ -6,11 +6,9 @@ from kuibit.grid_data import UniformGrid
 import matplotlib.animation as animation
 import matplotlib.cm as cm
 from tqdm import tqdm
+import plot_info as pinf
 
 # Constants
-sims_dir = "/home/jolivera/simulations"
-saveplotdir = '/home/jolivera/python/plots'
-GR_dir = sims_dir + "/BDCollN/output-0000/output_directory"
 
 G = 6.673e-11       # m^3/(kg s^2)
 c = 299792458       # m/s
@@ -18,13 +16,20 @@ M_sol = 1.98892e30  # kg
 M_to_ms = 1. / (1000 * M_sol * G / (c * c * c))
 M_to_density = c**5 / (G**3 * M_sol**2)  # kg/m^3
 
-cmap = cm.gist_rainbow
-fig_width, fig_height = 6, 5
 
 
+if len(sys.argv) < 3:
+    print("Usage: python3 2dxy.py computer_name sim_name var fps")
+    sys.exit(1)
+
+current_computer = sys.argv[1]
+sim_name = sys.argv[2]
 
 
-dirGw= sims_dir + "/DefColl" 
+home_dir, sim_dir = pinf.IDcomputer(current_computer)
+dirGw= sim_dir + sim_name 
+current_dir = os.getcwd()
+saveplotdir = current_dir+"/pdfplots/"
 
 sim = sd.SimDir(dirGw)
 psi4 = sim.gws
@@ -38,8 +43,8 @@ plt.plot(psi4.t, np.real(psi4.y),  label = r"$\Psi_4^{2,2}$ Real Part at r ="+st
 plt.xlabel(r"$t/M_{sun}$")
 plt.ylabel(r"$\Psi_4$")
 plt.legend()
-apply_second_xaxis(plt.gca())
+pinf.apply_second_xaxis(plt.gca())
 
-figf_name = f"mp_Psi4_l2_m2_r{radius}"
+figf_name = f"{saveplotdir}{sim_name}_Psi4_l2_m2_r{radius}"
 fig.savefig(figf_name+".png")
 fig.savefig(figf_name+".pdf")
