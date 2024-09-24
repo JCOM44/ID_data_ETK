b07ea742711dc4e107f97fffb57cb2d9f6892a9d
JCOM44 <olvera.josc.44@gmail.com>
2024-09-24 11:00:25 +0200

    fix ghostzones in setting ID

--- JordanBSSN/ScalarInit/src/ID_SF_calc_Read2D.c
+++ JordanBSSN/ScalarInit/src/ID_SF_calc_Read2D.c
@@ -31,7 +31,7 @@ ID_SF_Read2D (CCTK_ARGUMENTS)
   DECLARE_CCTK_ARGUMENTS;
   DECLARE_CCTK_PARAMETERS;
 
-  CCTK_INFO("=== EBegin ID_SF_Read initial data ===");
+  CCTK_INFO("=== Begin ID_SF_Read initial data ===");
 
   /*=== define parameters ====================================*/
   /*==========================================================*/
@@ -39,7 +39,6 @@ ID_SF_Read2D (CCTK_ARGUMENTS)
 
   /*Reading related parameters */
 int m,s;
-/*char filename[] = "/home/jolivera/python/phi.h5";*/
 int sdiv,mdiv;
 double  betaphi,axes_ratio,r_e;
 double *s_gp,*mu_gp;
@@ -57,6 +56,7 @@ double **phi_td;
 
       phi_td = array_allocateSF(1,SDIV,1,MDIV);
 
+//   CCTK_INFO("=== Memory allocated ===");
 
       /* INITIALIZE VARIABLES WITH ZERO */
 
@@ -65,12 +65,13 @@ double **phi_td;
 	phi_td[s][m] = 0.0e0;
 		}
 
+//CCTK_INFO("Array initialized");
 /*=============Read from the HDF5 file and store in local variables====*/
 hdf5_read_varSF(&sdiv,&mdiv,SFmodel_file,&axes_ratio,&r_e,&betaphi,s_gp,mu_gp,phi_td);
 
+//CCTK_INFO("H5 file read");
 
 int i,j,k;
-int ind;
 int nx=cctkGH->cctk_lsh[0]; int ny=cctkGH->cctk_lsh[1]; int nz=cctkGH->cctk_lsh[2];
 
  CCTK_REAL *x_coord=0, *y_coord=0, *z_coord=0;  
@@ -89,42 +90,38 @@ double x_i,y_j,z_k;
 for(i=1;i<=nx;i++)
         for(j=1;j<=ny;j++)
                 for(k=1;k<=nz;k++) {
-			ind = i-1+nx*(j-1+ny*(k-1));
-
+			int ind = i-1+nx*(j-1+ny*(k-1));			
 			grid_interpSF(phi_td,s_gp,mu_gp,r_e,nx,ny,nz,x_coord,y_coord,z_coord,i,j,k,&phi_ijk,1);
 		/*	if(phi_ijk==0.0){
 				phi_ijk = phi_at_inf; 	
 			}*/
-
 			phi1[ind] = phi_ijk;
-			/*phi2[ind] = 0.0;*/
-			Kphi1[ind] = 0.0;
-			/*Kphi2[ind] = 0.0;*/
-
+			Kphi1[ind] = 0.0;			
 					}
 
-  CCTK_INFO("=== End ID_SF_Read2D initial data ===");
- /*=========== derivatives and advective derivatives ====*/
+
+ //=========== derivatives and advective derivatives ====
   CCTK_REAL     dphi_x,dphi_y,dphi_z;
 
   CCTK_REAL dx12,dy12,dz12;
 
   CCTK_INFO("=== Setting differences... ===");
-  /*========= Define delta_xi =========*/
+  //========= Define delta_xi =========
   dx12 = 12.0*CCTK_DELTA_SPACE(0);
   dy12 = 12.0*CCTK_DELTA_SPACE(1);
   dz12 = 12.0*CCTK_DELTA_SPACE(2);
 
-printf("\n%f",dx12);
-printf("\n%f",dy12);
-printf("\n%f",dz12);
+
   CCTK_INFO("=== Calculating bounds ... ===");
   
+
   CCTK_INT imin[3], imax[3];
   for (int d = 0; d < 3; ++ d)
   {
-    imin[d] = 0;
-    imax[d] = cctk_lsh[d];
+//    imin[d] = 0;
+//    imax[d] = cctk_lsh[d];
+      imin[d] = cctk_nghostzones[d];
+      imax[d] = cctk_lsh[d] - cctk_nghostzones[d];
   }
   for (int k = imin[2]; k < imax[2]; ++k)
   {
@@ -137,7 +134,8 @@ printf("\n%f",dz12);
 
 
 
-  /*====== calculate derivatives =======*/
+
+  //====== calculate derivatives =======/
 
 
 
@@ -157,20 +155,18 @@ printf("\n%f",dz12);
                 + phi1[CCTK_GFINDEX3D (cctkGH, i, j, k-2)]   ) / dz12;
 
 
-	if (CCTK_EQUALS (theory, "BDdecouplingEF")){
-    Kphi1[ind] = 1/(2*alp[ind]) * (betax[ind] * dphi_x 
+   Kphi1[ind] =   1/(alp[ind])  * (betax[ind] * dphi_x 
                          + betay[ind] * dphi_y
-                        + betaz[ind] * dphi_z );
-						}
-	else    {
+			                   + betaz[ind] * dphi_z );
 
-    Kphi1[ind] = 1/(alp[ind]) * (betax[ind] * dphi_x 
-                         + betay[ind] * dphi_y
-                        + betaz[ind] * dphi_z );
-		}
+		
         }
      }
-  }
-
+  } 
+  free(s_gp);
+  free(mu_gp);
+  array_freeSF(phi_td, 1, 1);
+  CCTK_INFO("=== ALL SET ... ===");
+  CCTK_INFO("=== End ID_SF_Read2D initial data ===");
 }
 /* -------------------------------------------------------------------*/
